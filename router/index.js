564f519c11a25a174a63db5c758d652590186855
allenou <oyqxiao@sina.com>
2020-12-13 21:28:20 +0800

    deploy

--- router/index.js
+++ router/index.js
@@ -1,46 +1,4 @@
 export default [
-    // {
-    //     title: '字符串倒序',
-    //     link: '/posts/string-reverse',
-    //     drafts: false,
-    //     date: '2017-02-21'
-    // },
-    // {
-    //     title: '重新认识 :last-child',
-    //     link: '/posts/last-child',
-    //     drafts: false,
-    //     date: '2016-01-12'
-    // },
-    // {
-    //     title: 'select 实现超链接效果',
-    //     link: '/posts/select-links',
-    //     drafts: true,
-    //     date: '2016-05-07'
-    // },
-    // {
-    //     title: '5 个你不知道的 HTML5 API',
-    //     link: '/posts/more-html5-apis',
-    //     drafts: false,
-    //     date: '2016-06-18'
-    // },
-    // {
-    //     title: '过滤空格',
-    //     link: '/posts/string-trim',
-    //     drafts: false,
-    //     date: '2016-06-18'
-    // },
-    // {
-    //     title: 'flexible.js 原理',
-    //     link: '/posts/flexible.js',
-    //     drafts: false,
-    //     date: '2019-04-24'
-    // },
-    // {
-    //     title: '对象转数组',
-    //     link: '/posts/converting-an-object-to-an-array',
-    //     drafts: false,
-    //     date: '2018-11-19'
-    // },
     {
         title: '小规矩',
         link: '/posts/small-rules',
@@ -48,8 +6,8 @@ export default [
         date: '2016-07-18'
     },
     {
-        title: '写点什么',
-        link: '/posts/write-something',
+        title: '那就写点什么',
+        link: '/posts/just-want-to-write-something',
         drafts: false,
         date: '2020-10-14'
     },
@@ -59,54 +17,16 @@ export default [
         drafts: false,
         date: '2020-10-18'
     },
+    {
+        title: '千万不要找小气鬼做房东',
+        link: '/posts/advice',
+        drafts: false,
+        date: '2020-12-12'
+    },
     // {
-    //     title: 'BFC',
-    //     link: '/posts/bfc',
+    //     title: '变态的买家',
+    //     link: '/posts/abnormal-buyer',
     //     drafts: false,
-    //     date: '2017-01-18'
-    // },
-    // {
-    //     title: '盒模型',
-    //     link: '/posts/box-model',
-    //     drafts: true,
-    //     date: '2019-04-19'
-    // },
-    // {
-    //     title: '日期格式与时间戳互转',
-    //     link: '/posts/date-and-timestamps',
-    //     drafts: false,
-    //     date: '2016-04-26'
-    // },
-    // {
-    //     title: 'table 合并单元格',
-    //     link: '/posts/merge-table-cells',
-    //     drafts: true,
-    //     date: '2016-02-20'
-    // },
-
-    // {
-    //     title: '子页面传值给父页面',
-    //     link: '/posts/pass-child-page-value-to-parent',
-    //     drafts: false,
-    //     date: '2016-05-09'
-    // },
-    // {
-    //     title: '原型',
-    //     link: '/posts/javascript-prototype',
-    //     drafts: true,
-    //     date: '2019-02-18'
-    // },
-    // {
-    //     title: '安卓端微信点击图片被放大问题',
-    //     link: '/posts/android-wechat-image',
-    //     drafts: true,
-    //     date: '2019-06-09'
-    // },
-    // {
-    //     title: '判断滚动条滚动方向',
-    //     link: '/posts/determine-the-scrolling-direction',
-    //     drafts: false,
-    //     date: '2019-06-09'
-    // },
-
+    //     date: '2020-12-13'
+    // }
 ]
